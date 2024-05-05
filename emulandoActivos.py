10ab6731737f1701a88de23ab3ea48e8ad8ba405
Luis Alberto Pacheco Espinoza <luis.pachecoe@alumno.buap.mx>
2024-05-05 17:05:57 -0600

    descargando activos para crear carteras

---
+++ emulandoActivos.py
@@ -0,0 +1,34 @@
+import pandas as pd # Importamos Pandas y para simplificar le asignamos el nombre pd
+
+from pandas_datareader import data as pdr
+import yfinance as yf
+yf.pdr_override() # Importamos Pandas Datareader y lo parcheamos para que funcione con Yahoo
+
+import numpy as np # Importamos Numpy y le asociamos el nombre np
+
+import datetime # Importamos datetime pero en este caso no vamos a asignarle ningún nombre
+
+# Para graficar correctamente en el notebook hacemos lo siguiente
+# esto hace que los gráficos se visualicen en el notebook
+import matplotlib.pyplot as plt
+import matplotlib.pylab as pylab
+pylab.rcParams['figure.figsize'] = 20, 10 # Definimos el tamaño de los gráficos para que se ajusten bien al notebook
+
+start = datetime.datetime(2017, 1, 1)
+end=datetime.datetime(2017, 12, 31)
+#tickers=['SAN.MC', 'BBVA.MC', 'TEF.MC', 'ITX.MC', 'REP.MC', 'ACS.MC', 'FER.MC', 'GRF.MC', 'IAG.MC', 'AMS.MC'] #datos de santander, bbva, telefonica, inditex, repsol, acs, ferrovial, grifols, iag, amadeus
+dataSantander=pdr.get_data_yahoo('SAN.MC', start=start, end=end)
+dataBBVA=pdr.get_data_yahoo('BBVA.MC', start=start, end=end)
+dataTelefonica=pdr.get_data_yahoo('TEF.MC', start=start, end=end)
+dataInditex=pdr.get_data_yahoo('ITX.MC', start=start, end=end)
+dataRepsol=pdr.get_data_yahoo('REP.MC', start=start, end=end)
+
+
+#print(dataSantander.head())
+#imprimiendo los datos de cierre de las acciones de cada mes de santander
+print(dataSantander['Close'].resample('M').last())
+print(dataBBVA['Close'].resample('M').last())
+print(dataTelefonica['Close'].resample('M').last())
+print(dataInditex['Close'].resample('M').last())
+
+
